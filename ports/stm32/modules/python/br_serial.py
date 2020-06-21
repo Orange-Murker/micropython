44bbf98263e66d6311195bbca85498d37e1183d0
Alex <alex.overbeek@hotmail.com>
2020-06-21 14:01:50 +0200

    .set() and .send() methods added for HIDscope-ish communication

--- ports/stm32/modules/python/br_serial.py
+++ ports/stm32/modules/python/br_serial.py
@@ -1,7 +1,7 @@
 # Biorobotics non-blocking serial TX over CN1 (ST-Link VCOM, UART3) on STM32 H743ZI2
 
 # Imports
-import machine, stm, pyb
+import machine, stm, pyb, utime
 
 # Imported C function that contains non-blocking uart3br.send()
 # See uart3br.c in micropython external C modules folder
@@ -13,11 +13,36 @@ uart_pc = pyb.UART(3, 115200)
 # Disable the default enabled useless RX IRQ
 machine.mem8[stm.USART3+stm.USART_CR1] &= ~(1 << 5)
 
-# Reserved adress in ram where to exchange data between micropython and C layer
-# Adresses incrementing of 1 means 1 8mem
-REG = 0x30040000
-REG_L = 16
+# # Reserved adress in ram where to exchange data between micropython and C layer
+# # Adresses incrementing of 1 means 1 8mem
+# REG = 0x30040000
+# REG_L = 16
 
-# Reset data exchange counters
-machine.mem32[REG+0]=0 # c
-machine.mem32[REG+4]=0 # n
+# # Reset data exchange counters
+# machine.mem32[REG+0]=0 # c
+# machine.mem32[REG+4]=0 # n
+
+class serial:
+
+    def __init__(self, nch):
+        self.nch = nch
+        self.data = nch*[0]
+        self.time = []
+
+    def set(self, ch, word):
+        self.data[ch] = float(word)
+
+    
+    def send(self):
+
+        # 3 identifier bytes, one byte channel number
+        uart3br.send(0x7FFFBF00^self.nch)
+
+        # Timestamp integer in microseconds
+        self.time = utime.ticks_us()
+        uart3br.send(self.time)
+
+        # Data
+        for word in self.data:
+            uart3br.send(word) 
+    
