6642571b3d2647581fedde9698aef5b7af8efe23
Alex <alex.overbeek@hotmail.com>
2020-06-23 12:20:22 +0200

    Uncommented resetting the exchange counters, otherwise random amounts of sending

--- ports/stm32/modules/python/br_serial.py
+++ ports/stm32/modules/python/br_serial.py
@@ -13,14 +13,14 @@ uart_pc = pyb.UART(3, 115200)
 # Disable the default enabled useless RX IRQ
 machine.mem8[stm.USART3+stm.USART_CR1] &= ~(1 << 5)
 
-# # Reserved adress in ram where to exchange data between micropython and C layer
-# # Adresses incrementing of 1 means 1 8mem
-# REG = 0x30040000
-# REG_L = 16
+# Reserved adress in ram where to exchange data between micropython and C layer
+# Adresses incrementing of 1 means 1 8mem
+REG = 0x30040000
+REG_L = 16
 
-# # Reset data exchange counters
-# machine.mem32[REG+0]=0 # c
-# machine.mem32[REG+4]=0 # n
+# Reset data exchange counters
+machine.mem32[REG+0]=0 # c
+machine.mem32[REG+4]=0 # n
 
 class serial_pc:
 
