0ab7d89d6a808d3ec845f4344d4b5c76f2b39bfa
Roberto-R <robert.soor@gmail.com>
2020-06-27 15:55:05 +0200

    Basic scope is working

--- tools/uscope/main.py
+++ tools/uscope/main.py
@@ -1,20 +1,19 @@
 import sys
 from PyQt5.QtWidgets import *
+from PyQt5.QtGui import QIntValidator
 from PyQt5.QtCore import pyqtSlot, QIODevice
-from PyQt5.QtWidgets import QMessageBox
 from PyQt5.QtSerialPort import QSerialPort
 import pyqtgraph as pg
 import numpy as np
+from pyqtgraph import mkPen
 
 from plot_decoder import PlotDecoder
-
-# Basic PyQtGraph settings
-pg.setConfigOption('background', 'w')
-pg.setConfigOption('foreground', 'k')
 
 
 class MainWindow(QMainWindow):
     """Class for running the application window"""
+
+    LINECOLORS = ['y', 'm', 'r', 'g', 'c', 'w', 'b']
 
     def __init__(self, *args, **kwargs):
         """Constructor"""
@@ -23,6 +22,7 @@ class MainWindow(QMainWindow):
 
         layout_main = QVBoxLayout()  # Main vertical layout
         layout_top = QHBoxLayout()  # Layout for top buttons
+        layout_bottom = QVBoxLayout()  # Layout for channels
 
         # Port control
         layout_port = QFormLayout()
@@ -35,21 +35,29 @@ class MainWindow(QMainWindow):
             toggled=self.on_connect_toggle
         )
 
+        # Data size
+        self.input_size = QLineEdit()
+        self.input_size.setValidator(QIntValidator())
+        self.input_size.setText("500")
+        layout_port.addRow(QLabel("Samples:"), self.input_size)
+
+        # Overlay
+        self.input_overlay = QCheckBox()
+        layout_port.addRow(QLabel("Overlay channels:"), self.input_overlay)
+
         layout_top.addLayout(layout_port)
         layout_top.addWidget(self.button_port)
 
         layout_main.addLayout(layout_top)
 
         # Plots
-        self.plot = pg.PlotWidget()
-        self.plot.showGrid(True, True)
+        self.layout_plots = pg.GraphicsLayoutWidget()
+        layout_bottom.addWidget(self.layout_plots)
 
-        self.p1 = self.plot.getPlotItem()
+        self.plots = []  # Start with empty plots
+        self.curves = []
 
-        self.console = QTextEdit(readOnly=True)
-
-        layout_main.addWidget(self.plot)
-        layout_main.addWidget(self.console)
+        layout_main.addLayout(layout_bottom)
 
         # Main window widget
         widget = QWidget()
@@ -67,7 +75,12 @@ class MainWindow(QMainWindow):
 
         # Prepare data structure
         self.channels = 0  # Wait for serial data, resize on the fly
-        self.data = None
+        self.data = None  # Received data, each row is a channel
+        self.time = None  # Timestamps of each data column
+        self.data_points = 0  # Number of points recorded
+        self.data_size = 1  # Number of points in history
+        self.time_offset = None  # Time offset in microseconds
+        self.overlay = False  # When true, all plots should be combined in one plot
 
         self.set_channels(self.channels)
 
@@ -82,13 +95,18 @@ class MainWindow(QMainWindow):
         if checked:
             port = self.input_port.text()
             self.serial.setPortName(port)
-            if self.serial.open(QIODevice.ReadOnly):
+            if self.serial.open(QIODevice.ReadOnly):  # If serial opened successfully
                 self.input_port.setDisabled(True)
+                self.input_size.setDisabled(True)
+                self.input_overlay.setDisabled(True)
+                self.start_recording()
             else:
                 self.button_port.setChecked(False)  # Undo toggle
                 QMessageBox.warning(self, 'Serial connection', 'Could not connect to device', QMessageBox.Ok)
         else:
             self.input_port.setDisabled(False)
+            self.input_size.setDisabled(False)
+            self.input_overlay.setDisabled(False)
 
     @pyqtSlot()
     def on_serial_receive(self):
@@ -98,6 +116,14 @@ class MainWindow(QMainWindow):
         if self.decoder.receive(byte):
             self.update_data(self.decoder.channel_size, self.decoder.time, self.decoder.data)
 
+    def start_recording(self):
+        """Called when recording should start (e.g. when `connect` was hit)"""
+        self.channels = 0  # Force an update on the next data point
+        self.data_size = int(self.input_size.text())
+        self.overlay = self.input_overlay.isChecked()
+
+        self.serial.clear()  # Get rid of data in buffer
+
     def update_data(self, channels, time, new_data):
         """Called when new row was received"""
 
@@ -105,20 +131,92 @@ class MainWindow(QMainWindow):
             self.set_channels(channels)
 
         col = np.array(new_data, dtype=float)
+        self.data = np.roll(self.data, -1, axis=1)  # Rotate backwards
+        self.data[:, -1] = col[:, 0]  # Set new column at the end
 
-        self.data = np.concatenate((self.data, col))
+        self.time = np.roll(self.time, -1)  # Rotate backwards
 
-        print(self.data)
+        if self.time_offset is None:
+            self.time_offset = time
+
+        self.time[0, -1] = (time - self.time_offset) / 1000000
+
+        self.data_points += 1
+
+        self.update_plots()
+
+    def update_plots(self):
+        """With data already updated, update plots"""
+
+        if self.data_points < self.data_size:
+            data_x = self.time[:, -self.data_points:]
+            data_y = self.data[:, -self.data_points:]
+        else:
+            data_x = self.time
+            data_y = self.data
+
+        for i, curve in enumerate(self.curves):
+            curve.setData(x=data_x[0, :], y=data_y[i, :])
 
     def set_channels(self, channels):
-        """Resize number of channels"""
+        """
+        Resize number of channels
 
-        self.data = np.ndarray([channels, 1], dtype=float)
+        Also functions as a reset between recordings, also sets new plot windows and curves
+        """
+
+        self.channels = channels
+        self.data = np.zeros((channels, self.data_size))
+        self.time = np.zeros((1, self.data_size))
+        self.time_offset = None
+        self.data_points = 0
+
+        self.create_plots()
+
+    def create_plots(self):
+        """Create the desired plots and curves"""
+
+        # Clear old
+        for plot in self.plots:
+            plot.clear()
+            self.layout_plots.removeItem(plot)
+
+        self.plots = []
+        self.curves = []
+
+        if self.overlay:
+            new_plot = self.layout_plots.addPlot(row=0, col=0, title="Channels")
+
+            for i in range(self.channels):
+                new_curve = new_plot.plot()
+                self.curves.append(new_curve)
+
+            self.plots.append(new_plot)
+
+        else:
+            for i in range(self.channels):
+                new_plot = self.layout_plots.addPlot(row=i, col=0, title="Ch{}".format(i))
+                new_plot.showGrid(False, True)
+
+                new_curve = new_plot.plot()
+
+                self.plots.append(new_plot)
+                self.curves.append(new_curve)
+
+        # Set style
+        for plot in self.plots:
+            plot.showGrid(False, True)
+
+        for i, curve in enumerate(self.curves):
+            c = self.LINECOLORS[i % len(self.LINECOLORS)]  # Set automatic colors
+            pen = mkPen(c, width=2)
+            curve.setPen(pen)
 
 
 # Run window when file was called as executable
 if __name__ == '__main__':
 
     app = QApplication([])
+    app.setStyle('Fusion')
     window = MainWindow()
     sys.exit(app.exec())
