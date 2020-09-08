43defbeb85ba95a22fb900b46d485c8c26aad36d
Roberto-R <robert.soor@gmail.com>
2020-09-08 09:12:55 +0200

    Got embedded modules to work!

--- ports/windows/modules/python/mypackage.py
+++ ports/windows/modules/python/mypackage.py
@@ -1,6 +1,14 @@
 
 class MyClass:
     """Test class that does nothing"""
+
+    test = 59646
     
-    def __init__(self):
-        pass
+    def __init__(self, value=1):
+        self.test = value
+
+    def get(self):
+        return self.test
+
+    def hello(self):
+        print("Hi there! This sis some greeting")
