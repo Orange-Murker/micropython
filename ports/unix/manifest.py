be5bd0dea0cc8d05cd02f68f24d84dd0c729a61d
Roberto-R <robert.soor@gmail.com>
2020-09-10 11:13:04 +0200

    Tried tricking VS Code with more modules, cleaned up manifests

--- ports/unix/manifest.py
+++ ports/unix/manifest.py
@@ -1,4 +1,4 @@
 freeze_as_mpy('$(MPY_DIR)/tools', 'upip.py')
 freeze_as_mpy('$(MPY_DIR)/tools', 'upip_utarfile.py', opt=3)
 
-freeze('$(MPY_DIR)/ports/windows/modules/python')
+include('$(MPY_DIR)/ports/windows/manifest.py')
